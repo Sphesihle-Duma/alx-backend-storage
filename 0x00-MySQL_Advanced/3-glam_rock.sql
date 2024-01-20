9321266513f9b755a1225db46347a31287aa105f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-20 20:54:46 +0000

    listing all bands with glam rock

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Lists all bands with glam rock and rank by longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan 
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC
